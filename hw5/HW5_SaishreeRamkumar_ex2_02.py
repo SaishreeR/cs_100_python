47ac9a360d7264fa080ff91ff4a6fe9da316e29a
Saishree Ramkumar <bhuvansaishree@gmail.com>
2024-02-29 23:32:47 -0500

    uptaded hw 5 2a & 2b

--- hw5/HW5_SaishreeRamkumar_ex2_02.py
+++ hw5/HW5_SaishreeRamkumar_ex2_02.py
@@ -1,19 +1,47 @@
 """
 Saishree Ramkumar 
 CS 100 Room CKB 207
-HW 5 Excercise 2 28 Feb 2024 10:53 pm est
+HW 5 Excercise 2 29 Feb 2024 9:41 pm est
 """
 
 # Importing package
 
-inputLetter1 = 'R'
-inputLetter2 = 's'
-inputLetter3 = ''
-lst1 = ['A', 'B', 'C', 'F']
-lst2 = ['a', 'b', 'c', 'h']
-lst3 = []
 
-print("***Q1***")
-#def isDivisible(maxInt, twoInts):
-    #for intIndex in range(maxInt):
+
+
+print("***Q2-A***")
+def isDivisible(maxInt, twoInts):
+    lstReturn = []
+    print("Max int value: " + str(maxInt))
+    print("Tuple value : " + str(twoInts))
+    for intMaxIndex in range(1, maxInt):
+        #print(intMaxIndex)
+        intTotalCnt = 0
+        #lstReturn.append(intMaxIndex)
+        for intTpIndex in twoInts:
+            #print("Max index value: " + str(intMaxIndex))
+            #print(intTpIndex)
+            print("Answer = " + str(intMaxIndex) + "/" + str(intTpIndex) + ": " + str(intMaxIndex%intTpIndex))
+            if (intMaxIndex%intTpIndex) == 0:
+                intTotalCnt = intTotalCnt + 1
+        if (intTotalCnt == len(twoInts)):
+            lstReturn.append(intMaxIndex)
+    print("Divisible by tuple: ")
+    print(lstReturn)
+
+#Calling the function
+print("***TEST CASE 1***")
+maxIntValue = 7
+tpTwoValue = (2, 3)
+isDivisible(maxIntValue, tpTwoValue)
+print("***Q2-B***")
+print("***TEST CASE 2***")
+maxIntValue = 6
+tpTwoValue = (2, 3)
+isDivisible(maxIntValue, tpTwoValue)
+print("***TEST CASE 3***")
+maxIntValue = 14
+tpTwoValue = (2, 3)
+isDivisible(maxIntValue, tpTwoValue)
+        
         
